769baf80bb6d9996bf8ba80e864c860b6a9102e9
Davi Alves Froza <alvesfrozadavi@gmail.com>
2025-03-29 20:17:04 -0300

    Delete parametros directory

--- parametros/parametros_proj/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from ponderacao import urls, views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tabela-parametros/', views.tabela_parametros, name='tabela_parametros'),
-]
-
--- parametros/ponderacao/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('tabela-parametros/', views.tabela_parametros, name='tabela_parametros'),
-]
