f6e6f86693b934b6ed1f2caa03f1be27829b6d4b
Davi Alves Froza <alvesfrozadavi@gmail.com>
2025-04-01 17:30:20 -0300

    Delete Site directory

--- Site/sustentabilidade_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UsuarioSustentavel
-
-class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model = UsuarioSustentavel
-        fields = ['nome', 'consumo_energia', 'consumo_agua', 'residuos', 'uso_transporte']
