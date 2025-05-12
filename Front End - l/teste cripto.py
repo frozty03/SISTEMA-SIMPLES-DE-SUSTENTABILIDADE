508998c98b37662838c667a5281296908b2e3bd4
nizmaiaj <juju.ginastica1@gmail.com>
2025-05-12 10:51:47 -0300

    Atualizar o teste cripto.py

--- Front End - l/teste cripto.py
+++ Front End - l/teste cripto.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# dicionário
+# dicionário que associa cada caractere a um numero 
 mapeamento = {
     # números
     '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
@@ -12,18 +12,22 @@ mapeamento = {
     '-': 36, '.': 37, ' ': 38, '$': 39, '/': 40, '+': 41, '%': 42
 }
 
-# para decodificação
+# para decodificação// permite voltar numerp para caractere
 inverso = {v: k for k, v in mapeamento.items()}
 
-# matriz de criptografia
+# matriz de criptografia // usada para embaralhar cada par de numeros
 A = np.array([[5, 4], [3, 3]])
 
 
 # função para calcular a matriz inversa modular
 def matriz_inversa_modular(matriz, modulo):
+    #calcula o determinante (0-42)
     det = int(np.round(np.linalg.det(matriz))) % modulo
+    #inverso do determinante usado para desembaralhar 
     det_inverso = pow(det, -1, modulo)
+    
     matriz_adjunta = np.array([[matriz[1, 1], -matriz[0, 1]], [-matriz[1, 0], matriz[0, 0]]])
+    #multiplica adjunta pelo inverso do determinado 
     return (det_inverso * matriz_adjunta) % modulo
 
 
@@ -32,10 +36,12 @@ AI = matriz_inversa_modular(A, 43)
 
 
 def codificar(mensagem):
+    #passa tudo pars letra minúscula
     mensagem = mensagem.upper()
+    
     caracteres_validos = set(mapeamento.keys())
 
-    # verifica caracteres inválidos
+    # verifica se os caracteres estao no dicionario
     for c in mensagem:
         if c not in caracteres_validos:
             raise ValueError(f"Caractere inválido: '{c}'")
@@ -44,17 +50,19 @@ def codificar(mensagem):
     if len(mensagem) % 2 != 0:
         mensagem += mensagem[-1]
 
-    # converter para números
+    # converter cada caractere para números
     I = [mapeamento[c] for c in mensagem]
 
     # criar matriz de pares
     P = []
     for i in range(len(I) // 2):
         k = 2 * i
+        #adiciona o valor na lista P
         P.append([I[k], I[k + 1]])
+        
     P = np.array(P).T
 
-    # codificar
+    # codificar // multiplica cada par pela matriz A
     C = np.dot(A, P) % 43
 
     # converter para texto
@@ -82,7 +90,7 @@ def decodificar(codificado):
         C.append([I[k], I[k + 1]])
     C = np.array(C).T
 
-    # decodificar
+    # decodificar / multiplica pela matriz inversa 
     P = np.dot(AI, C) % 43
 
     # converter para texto
@@ -92,7 +100,7 @@ def decodificar(codificado):
         TC.extend(P[i, :])
     decodificado = [inverso[int(i)] for i in TC]
 
-    # Verifica se o último caractere foi repetido (padding)
+    # Verifica se o último caractere foi repetido (padding) se for será removido
     if len(decodificado) > 1 and decodificado[-1] == decodificado[-2]:
         decodificado = decodificado[:-1]
 
